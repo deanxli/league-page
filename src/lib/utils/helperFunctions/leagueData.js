cc8066fa376081e95126d8bfb35bef8d591a5262
Dean Li <deanxli@gmail.com>
2021-08-03 10:37:35 -0400

    Update leagueData.js

--- src/lib/utils/helperFunctions/leagueData.js
+++ src/lib/utils/helperFunctions/leagueData.js
@@ -1,8 +1,8 @@
 import { get } from 'svelte/store';
 import {leagueData} from '$lib/stores';
 
-export const leagueID = "your_league_id"; // your league ID
-export const leagueName = "your_league_name"; // your league name
+export const leagueID = "652567042582491136"; // your league ID
+export const leagueName = "Entourage Dynasty League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 
